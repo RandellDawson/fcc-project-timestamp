fa5b73b7d31430d39af5cc48fb061d184c4a33b3
Randell Dawson <randy@randydawson.com>
2022-08-04 09:43:43 -0600

    fix: completed all user stories

--- index.js
+++ index.js
@@ -5,20 +5,37 @@ const app = express();
 // so that your API is remotely testable by FCC 
 
 const cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static('public'));
+
+const returnDates = (res, date) => {
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+};
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", (_, res) => {
+  returnDates(res, new Date(Date.now()));
+});
+
 app.get("/api/:date", (req, res) => {
   const submittedDate = req.params.date;
-  console.log(submittedDate);
-  const date = submittedDate ? new Date(submittedDate) : new Date();
-  console.log(date);
-  res.json({date});
+  let date = new Date(submittedDate);
+  if (date.toString() === "Invalid Date") {
+    date = new Date(Number(submittedDate)); // assumes date input is in milliseconds
+    if (date.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    }
+    else {
+      returnDates(res, date);
+    }
+  }
+  else {
+    returnDates(res, date);
+  }
 });
 
 const listener = app.listen(process.env.PORT, () => {
